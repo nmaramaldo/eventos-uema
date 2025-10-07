1f8a136937b5de04291fa67e6539bc8f4cdb3d9d
MAC <62212909+MacUpr@users.noreply.github.com>
2025-10-07 18:28:46 -0300

    asterisco anadido para campos obrigatorios

--- resources/js/components/ui/BaseInput.vue
+++ resources/js/components/ui/BaseInput.vue
@@ -1,24 +1,31 @@
 <script setup>
 const props = defineProps({
-  id: String,
-  label: String,
-  modelValue: [String, Number],
-  type: { type: String, default: 'text' },
-  placeholder: String,
+  id: String,
+  label: String,
+  modelValue: [String, Number],
+  type: { type: String, default: 'text' },
+  placeholder: String,
+  // 1. ADICIONE A PROPRIEDADE 'REQUIRED' AQUI
+  required: {
+    type: Boolean,
+    default: false
+  }
 })
 const emit = defineEmits(['update:modelValue'])
 </script>
 
 <template>
-  <div class="space-y-1">
-    <label v-if="label" :for="id" class="form-label">{{ label }}</label>
-    <input
-      :id="id"
-      :type="type"
-      :value="modelValue"
-      :placeholder="placeholder"
-      class="form-input"
-      @input="emit('update:modelValue', $event.target.value)"
-    />
-  </div>
+  <div class="space-y-1">
+        <label v-if="label" :for="id" class="form-label">
+      {{ label }} <span v-if="required" class="text-red-500">*</span>
+    </label>
+    <input
+      :id="id"
+      :type="type"
+      :value="modelValue"
+      :placeholder="placeholder"
+      class="form-input"
+      @input="emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
