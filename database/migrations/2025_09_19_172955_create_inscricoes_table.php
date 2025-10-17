32a1ed9cc3bf2b361eb06c666205eed0324381ff
Natalia <nataliamaramaldof@gmail.com>
2025-10-17 09:43:50 -0300

    Corrige erro de FOREIGN KEY ao excluir eventos

--- database/migrations/2025_09_19_172955_create_inscricoes_table.php
+++ database/migrations/2025_09_19_172955_create_inscricoes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('inscricoes', function (Blueprint $table) {
             $table->uuid('id')->primary(); 
             $table->foreignUuid('user_id')->constrained('users');
-            $table->foreignUuid('evento_id')->constrained('eventos');
+            $table->foreignUuid('evento_id')->constrained('eventos')->cascadeOnDelete();;
             $table->string('status');
             $table->timestamp('data_inscricao')->useCurrent();
             $table->boolean('presente')->default(false);
